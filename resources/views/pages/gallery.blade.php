befccb1b771e742ca90d03f42f0a6c5d31a6024d
Waclaw Pich <szymmonlewan@gmail.com>
2026-07-22 19:28:40 +0200

    gallery css fix

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -50,11 +50,11 @@
         <ul>
             @foreach ($labels as $label)
                 <li>
-                    <a href="/gallery2?label={{ urlencode($label->label) }}" class="label-card-link">
+                    <a href="/gallery2?label={{ urlencode($label->label ?? '') }}" class="label-card-link">
                         <div class="gallery-item-wrapper">
                             <span class="label-card-title">{{ $label->label }}</span>
                             <div class="gallery-item">
-                                <img src="{{ asset($label->path) }}" alt="{{ $label->label }}" class="img-fluid">
+                                <img src="{{ asset(ltrim($label->path ?? '', '/')) }}" alt="{{ $label->label }}" class="img-fluid">
                             </div>
                             <span class="label-card-title">{{ Carbon::parse($label->latest_updated_at)->locale('pl')->translatedFormat('d M Y H:i') }}</span>
                         </div>
